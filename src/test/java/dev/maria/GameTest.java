515513fd66b167fa17ad916d4eff7978a1dce9e4
Maria Garcia Sierra <mgarciasierragrsr@gmail.com>
2024-11-17 20:35:13 +0100

    Added test cases for "Lagarto" and "Spock" in GameTest.java.

--- src/test/java/dev/maria/GameTest.java
+++ src/test/java/dev/maria/GameTest.java
@@ -21,20 +21,36 @@ public class GameTest {
         assertEquals("Empate", Game.winnerDetermination("Piedra", "Piedra"));
         assertEquals("Empate", Game.winnerDetermination("Papel", "Papel"));
         assertEquals("Empate", Game.winnerDetermination("Tijera", "Tijera"));
+        assertEquals("Empate", Game.winnerDetermination("Lagarto", "Lagarto"));
+        assertEquals("Empate", Game.winnerDetermination("Spock", "Spock"));
     }
 
     @Test
     public void testWinner() {
         assertEquals("Winner", Game.winnerDetermination("Piedra", "Tijera"));
+        assertEquals("Winner", Game.winnerDetermination("Piedra", "Lagarto"));
         assertEquals("Winner", Game.winnerDetermination("Papel", "Piedra"));
+        assertEquals("Winner", Game.winnerDetermination("Papel", "Spock"));
         assertEquals("Winner", Game.winnerDetermination("Tijera", "Papel"));
+        assertEquals("Winner", Game.winnerDetermination("Tijera", "Lagarto"));
+        assertEquals("Winner", Game.winnerDetermination("Lagarto", "Spock"));
+        assertEquals("Winner", Game.winnerDetermination("Lagarto", "Papel"));
+        assertEquals("Winner", Game.winnerDetermination("Spock", "Tijera"));
+        assertEquals("Winner", Game.winnerDetermination("Spock", "Piedra"));
     }
 
     @Test
     public void testGameOver() {
-        assertEquals("Game Over", Game.winnerDetermination("Piedra", "Papel"));
-        assertEquals("Game Over", Game.winnerDetermination("Papel", "Tijera"));
-        assertEquals("Game Over", Game.winnerDetermination("Tijera", "Piedra"));
+    assertEquals("Game Over", Game.winnerDetermination("Piedra", "Papel"));
+    assertEquals("Game Over", Game.winnerDetermination("Piedra", "Spock"));
+    assertEquals("Game Over", Game.winnerDetermination("Papel", "Tijera"));
+    assertEquals("Game Over", Game.winnerDetermination("Papel", "Lagarto"));
+    assertEquals("Game Over", Game.winnerDetermination("Tijera", "Piedra"));
+    assertEquals("Game Over", Game.winnerDetermination("Tijera", "Spock"));
+    assertEquals("Game Over", Game.winnerDetermination("Lagarto", "Piedra"));
+    assertEquals("Game Over", Game.winnerDetermination("Lagarto", "Tijera"));
+    assertEquals("Game Over", Game.winnerDetermination("Spock", "Papel"));
+    assertEquals("Game Over", Game.winnerDetermination("Spock", "Lagarto"));
     }
 
     @Test
